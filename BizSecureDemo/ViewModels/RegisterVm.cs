87b91da4649d26e46f83cb07b5a1fb51a607d5fd
inf2066-usr <inf2066-usr@INF-2066-15>
2026-02-11 13:47:15 +0200

    Add project files.

---
+++ BizSecureDemo/ViewModels/RegisterVm.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BizSecureDemo.ViewModels;
+public class RegisterVm
+{
+    [Required, EmailAddress]
+    public string Email { get; set; } = "";
+    [Required, MinLength(6)]
+    public string Password { get; set; } = "";
+}
+
